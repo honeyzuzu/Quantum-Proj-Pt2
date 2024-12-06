f2006a7c3d061587e55b59a000318717dd8a1ef7
Nicholas Grablevsky <kolya46592@gmail.com>
2024-12-06 16:23:16 -0500

    Added python sample code for the problem

--- Python_sample_code.py
+++ Python_sample_code.py
@@ -44,3 +44,36 @@ annual_portfolio_return = (1 + simulated_returns["portfolio"]).prod() ** (
 ) - 1
 annual_portfolio_volatility = np.std(simulated_returns["portfolio"]) * np.sqrt(12)
 # calculate the Sharpe ratio
+
+risk_free_rate = 0.00
+sharpe_ratio = (annual_portfolio_return - risk_free_rate) / annual_portfolio_volatility
+# calculate the maximum drawdown
+cumulative_returns = (1 + simulated_returns["portfolio"]).cumprod()
+max_drawdown = np.min(
+    cumulative_returns / np.maximum.accumulate(cumulative_returns) - 1
+)
+# calculate the Calmar ratio
+calmar_ratio = annual_portfolio_return / max_drawdown
+
+# Rebalance the portfolio quarterly based on the historical data
+# Similar approach can be used to calculate the portfolio performance for other rebalancing frequencies
+# such as monthly, semi-annually, and annually
+# The approach can also be used to calculate the portfolio performance for simulated data.
+quarterly_returns = historic_data.iloc[:, 1:4]
+quarterly_returns = (1 + quarterly_returns).cumprod()
+dates = historic_data["Date"]
+months = pd.DatetimeIndex(dates).month
+quarter_ends = np.where(months % 3 == 0)[0]
+quarterly_returns = quarterly_returns.iloc[quarter_ends]
+quarterly_returns.loc[-1] = 1
+quarterly_returns = quarterly_returns.sort_index()
+quarterly_returns = quarterly_returns.pct_change()
+quarterly_returns = quarterly_returns.dropna()
+quarterly_returns = quarterly_returns.reset_index(drop=True)
+
+# calculate the portfolio performance
+quarterly_returns["portfolio"] = quarterly_returns.dot(weights)
+annual_portfolio_return = (1 + quarterly_returns["portfolio"]).prod() ** (
+    4 / quarterly_returns.shape[0]
+) - 1
+annual_portfolio_volatility = np.std(simulated_returns["portfolio"]) * np.sqrt(4)
